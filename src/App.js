6522581d54d58a9965b539958c18b1cf7b04cb35
Opeyemi <yemilokoro@gmail.com>
2023-03-08 13:06:27 -0500

    installed latest version of bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by
-        <a href="https://github.com/CodeNameLola" target="blank">
-          Lola Okoro{" "}
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/CodeNameLola/react-weather-app"
-          target="blank"
-        >
-          {""} open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by
+          <a href="https://github.com/CodeNameLola" target="blank">
+            Lola Okoro{" "}
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/CodeNameLola/react-weather-app"
+            target="blank"
+          >
+            {""} open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
