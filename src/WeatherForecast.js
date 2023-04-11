83a90bff67f4b1613adf0786b68855e2b3407dab
Opeyemi <yemilokoro@gmail.com>
2023-04-11 11:47:00 -0400

    removed weatherinfo details from weatherforecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,10 @@
 import React from "react";
-import WeatherInfo from "./WeatherInfo";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">
-          Wed
-          <WeatherInfo />
-          19 10
-        </div>
+        <div className="col">Wed 19 10</div>
       </div>
     </div>
   );
